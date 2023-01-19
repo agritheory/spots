294cf2cbda96a17ee20dde528e4d1d8bf599731c
Tyler Matteson <tyler@agritheory.com>
2023-01-19 09:20:18 -0500

    chore: remove conference pass doctype

--- spots/spots/doctype/conference_pass/test_conference_pass.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, SPOTS and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestConferencePass(FrappeTestCase):
-	pass
